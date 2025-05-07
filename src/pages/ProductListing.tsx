d33deb99213547ea7b7a3980cf3847ec6b7de72d
ushagiddaluri@gmail.com <ushagiddaluri@gmail.com>
2025-05-07 17:15:17 +0530

    Initial commit

--- src/pages/ProductListing.tsx
+++ src/pages/ProductListing.tsx
@@ -33,6 +33,62 @@ const sampleProducts: Product[] = [
     price: 499.99,
     description: 'Next-gen gaming console with 4K graphics',
     image: 'https://images.unsplash.com/photo-1486401899868-0e435ed85128?w=500&q=80'
+  },
+  {
+    id: '5',
+    name: 'Designer Handbag',
+    price: 149.99,
+    description: 'Elegant leather handbag for everyday use',
+    image:  'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSjWCfIqisL-VZnYHpxt6MjV32iXvCDED8-9JQY-QlCQy5oltggOm7EzmcYKDFa7W2WGqPhYfrx8uD99-KDdz_6fYknvhFtI9JpcFb2ByAuB8NgfPtzFL7lpg' 
+  },
+  {
+    id: '6',
+    name: 'Branded Tote Bag',
+    price: 89.99,
+    description: 'Spacious and stylish tote bag from a premium brand',
+    image: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTGIV_cVsIZn-UFABoiQ7Iu-pLVOolxWWIyeRbq7gihHRbxgKpzqyXmXidDSEaMBu1UdNq4goMuJySZt7CgO5Lw6jCe4NAzSnMCXmQ3arplosGhKFClQ7AgNQ'  // Tote Bag
+  },
+  {
+    id: '7',
+    name: 'Fashion Keychain Set',
+    price: 19.99,
+    description: 'Set of colorful and trendy keychains',
+    image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQP9NBhsFNNqxcXVFNTaxVKwGY6bCeD9vM6hEFsxQu2jjN9-erol37VJaLf97LMt4p7XCvQBhAvlgkPFY7iNT3Lkd04ITkypaZPkZxepOB4rpbyEO_3WFS1WA'  // Keychains
+  },
+  {
+    id: '8',
+    name: 'Leather Key Holder',
+    price: 24.99,
+    description: 'Compact and durable leather key holder',
+    image: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSqvBr3b3fJk8ledvCIAzAxr-TAVIuUmEzaOX4plzC4rCA4V-bsHu6e3qxBtQmFP6wca0AOp5Bpy1gHSnHYe2BB1-QKHcOa2l2GfsTJa89TqA30aHbr88Cybg'  // Key Holder
+  },
+  {
+    id: '9',
+    name: 'Classic Black Handbag',
+    price: 129.99,
+    description: 'A timeless black handbag that goes with every outfit',
+    image: 'https://m.media-amazon.com/images/I/71YPDlZOM6L._SY625_.jpg'
+  },
+  {
+    id: '10',
+    name: 'Luxury Designer Handbag',
+    price: 799.99,
+    description: 'A luxury handbag with a sleek, elegant design from a top designer',
+    image: 'https://m.media-amazon.com/images/I/713794mJmCL._SY625_.jpg'
+  },
+  {
+    id: '11',
+    name: 'Casual Tote Bag',
+    price: 59.99,
+    description: 'Comfortable and spacious tote bag for daily use',
+    image: 'https://m.media-amazon.com/images/I/51xCKhxYUaL._SX625_.jpg'
+  },
+  {
+    id: '12',
+    name: 'Vintage Leather Handbag',
+    price: 249.99,
+    description: 'A vintage leather handbag with an antique design',
+    image: 'https://m.media-amazon.com/images/I/61T8HU1uHkL._SY575_.jpg'
   }
 ];
 
